ee0bd0b4c4994cb3a648fdb7a6d577b362db2f7f
Issac Cabral <issaccabrall@gmail.com>
2023-02-01 09:50:05 -0300

    refactor: move folder

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,10 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { api } from "../../services/api";
-import CategorySelect from "../CategorySelect";
+import CategorySelect from "./CategorySelect";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
