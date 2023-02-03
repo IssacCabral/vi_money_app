e05c01714b8c33051e52fe9998fab3b8ba476ef3
Issac Cabral <issaccabrall@gmail.com>
2023-02-03 13:30:10 -0300

    feat: add SignIn component

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import { Container, ContainerLogin, WrapLogin } from "./style";
+import "./styles.css";
 
 import logoImg from "../../assets/favicon.png";
 import { useState } from "react";
@@ -8,13 +8,13 @@ const SignIn = () => {
   const [password, setPassword] = useState<string>("");
 
   return (
-    <Container>
-      <ContainerLogin>
-        <WrapLogin>
-          <form>
-            <span>Bem Vindo!</span>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">Bem Vindo!</span>
 
-            <span>
+            <span className="login-form-title">
               <img src={logoImg} alt="" />
             </span>
 
@@ -25,6 +25,7 @@ const SignIn = () => {
                 value={email}
                 onChange={(event) => setEmail(event.target.value)}
               />
+              <span className="focus-input" data-placeholder="Email"></span>
             </div>
 
             <div className="wrap-input">
@@ -34,11 +35,23 @@ const SignIn = () => {
                 value={password}
                 onChange={(event) => setPassword(event.target.value)}
               />
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">
+                Criar conta.
+              </a>
             </div>
           </form>
-        </WrapLogin>
-      </ContainerLogin>
-    </Container>
+        </div>
+      </div>
+    </div>
   );
 };
 
